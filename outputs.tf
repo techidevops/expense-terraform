f4e226aabbd8024ae39aa781f196e62477677eaf
mkr0205 <mkr0205@outlook.com>
2024-10-02 20:59:40 +0200

    terraform vpc module

---
+++ outputs.tf
@@ -0,0 +1,11 @@
+output "vpc_id" {
+  value = module.vpc.vpc_id
+}
+
+# output "az_info" {
+#   value = module.vpc.az_info
+# }
+
+# output "default_vpc_info" {
+#   value = module.vpc.default_vpc_info
+# }
